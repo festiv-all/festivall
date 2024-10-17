af3da002f05e240e33d73ef027173327b9d9aa41
Youngbo Shim <swhybeing@gmail.com>
2024-10-17 17:53:23 +0900

    event detail, buy-ticket page

---
+++ app/buy-tickets/[slug]/page.tsx
@@ -0,0 +1,84 @@
+import OrderSummary from "@/components/orders/OrderSummary";
+import ProductCard from "@/components/products/ProductCard";
+import { createClient } from "@/utils/supabase/server";
+import { categoriesOrder } from "@/utils/utils";
+import { Calendar, MapPin } from "lucide-react";
+import Image from "next/image";
+
+export default async function BuyTicketsPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const supabase = createClient();
+  const title = params.slug.replaceAll("-", " ");
+  const currentDate = new Date().toISOString();
+
+  const { data: event } = await supabase
+    .from("events")
+    .select("*, products(*, categories(*)), organizers(id, corp_name)")
+    .eq("title", title)
+    .single();
+  console.log(title);
+  console.log(event);
+  const products = event?.products;
+  const categories = categoriesOrder(
+    event?.products.map((pr) => pr.categories)
+  );
+
+  return (
+    <div className="container mx-auto max-w-5xl px-5 py-6 text-xs lg:text-sm">
+      {event ? (
+        <div>
+          <div className="mb-4 lg:mb-6 mt-6 lg:mt-8">
+            <div className="h-32 lg:h-64 relative">
+              <Image
+                src="/campswingit.jpg"
+                alt={event.title}
+                className="w-full relative object-cover rounded-md"
+                fill
+              />
+            </div>
+            <h1 className="text-base lg:text-lg font-bold mb-2 mt-3 text-gray-800">
+              {event.title}
+            </h1>
+            <div className="flex items-center text-gray-600 mb-1">
+              <Calendar className="w-3 h-3 lg:w-4 lg:h-4 mr-1 lg:mr-2" />
+              <span>{event.date_start}</span>
+            </div>
+            <div className="flex items-center text-gray-600">
+              <MapPin className="w-3 h-3 lg:w-4 lg:h-4 mr-1 lg:mr-2" />
+              <span>{event.venue}</span>
+            </div>
+          </div>
+
+          <div className="lg:flex lg:gap-8 mt-16">
+            <div className="lg:flex-grow">
+              {categories?.map((cate) => (
+                <div key={cate.title} className="mb-10 lg:mb-16">
+                  <h2 className="text-sm lg:text-base font-semibold mb-4 text-gray-800">
+                    {cate.title}
+                  </h2>
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {products
+                      ?.filter((pr) => pr.category_id === cate.id)
+                      .map((item) => (
+                        <ProductCard key={item.id} product={item} />
+                      ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <OrderSummary />
+          </div>
+        </div>
+      ) : (
+        <div className="text-center py-28">
+          <div>이벤트 정보가 없습니다.</div>
+          <div>주소를 다시 확인해 주세요.</div>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ app/event/[slug]/page.tsx
@@ -0,0 +1,128 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { createClient } from "@/utils/supabase/server";
+import {
+  Calendar,
+  Facebook,
+  Globe,
+  Instagram,
+  MapPin,
+  Ticket,
+  User,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function EventDetailPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const supabase = createClient();
+  const event_title = params.slug.replaceAll("-", " ");
+  const { data: event } = await supabase
+    .from("events")
+    .select("*,organizers(*)")
+    .eq("title", event_title)
+    .single();
+
+  return (
+    <div className="container mx-auto max-w-5xl px-4 py-8 md:pt-12 md:pb-36">
+      {event ? (
+        <Card className="overflow-hidden">
+          <div className="h-64 relative">
+            <Image
+              src="/campswingit.jpg"
+              alt={event.title}
+              className="w-full relative object-cover"
+              fill
+            />
+          </div>
+          <CardContent className="p-6">
+            <h1 className="text-md md:text-xl font-bold mb-4">{event.title}</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
+              <div className="space-y-2">
+                <div className="flex items-center text-sm md:text-md">
+                  <Calendar className="w-5 h-5 mr-2 text-gray-500" />
+                  <span>{event.date_start}</span>
+                  <span>{event.date_end}</span>
+                </div>
+                <div className="flex items-center text-sm md:text-md">
+                  <MapPin className="w-5 h-5 mr-2 text-gray-500" />
+                  <span>{event.venue}</span>s
+                </div>
+                <div className="flex items-center text-sm md:text-md">
+                  <User className="w-5 h-5 mr-2 text-gray-500" />
+                  <span>{event.organizers?.corp_name}</span>
+                </div>
+              </div>
+              <div className="space-y-2 md:space-x-4 md:text-right text-sm md:text-md">
+                <div>
+                  <a target="_blank" href="https://camp.swingit.kr/">
+                    <Button className="w-full md:w-auto" variant="outline">
+                      <Globe className="w-4 h-4 mr-2" />
+                      Visit Homepage
+                    </Button>
+                  </a>
+                </div>
+                <div>
+                  <Link
+                    href={`/buy-tickets/${event.title.replaceAll(" ", "-")}`}
+                  >
+                    <Button className="w-full md:w-auto">
+                      <Ticket className="w-4 h-4 mr-2" />
+                      Buy Tickets
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <Separator className="my-6" />
+            <div className="space-y-4">
+              <h2 className="text-md md:text-xl font-semibold">
+                About the Event
+              </h2>
+              <p className="text-gray-700 text-sm md:text-md">
+                {event.description}
+              </p>
+            </div>
+            <Separator className="my-6" />
+            <div>
+              <h2 className="text-md md:text-xl font-semibold mb-4">
+                Connect with us
+              </h2>
+              <div className="flex space-x-4">
+                <Button variant="outline" size="icon" asChild>
+                  <a
+                    href={event.link_facebook || "#"}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Facebook className="w-4 h-4" />
+                    <span className="sr-only">Facebook</span>
+                  </a>
+                </Button>
+                <Button variant="outline" size="icon" asChild>
+                  <a
+                    href={event.link_instagram || "#"}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Instagram className="w-4 h-4" />
+                    <span className="sr-only">Instagram</span>
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      ) : (
+        <div className="text-center py-28">
+          <div>이벤트 정보가 없습니다.</div>
+          <div>주소를 다시 확인해 주세요.</div>
+        </div>
+      )}
+    </div>
+  );
+}
