38e9f20debbbe353f40a6f3ada86db31b36963db
festiv-all <swhybeing@gmail.com>
2024-10-18 13:05:59 +0900

    product type

--- lib/types/product.ts
+++ lib/types/product.ts
@@ -1,4 +1,9 @@
 import { Tables } from "./database.types";
 
-export type Product = Tables<"products">;
+export type Product = Tables<"products"> & {
+  categories: Tables<"categories"> | null;
+} & {
+  sub_categories: Tables<"sub_categories"> | null;
+};
+
 export type Category = Tables<"categories">;
