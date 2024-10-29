e1b8bddfb35d8fd47d9c1764259fa041feceafdd
festiv-all <swhybeing@gmail.com>
2024-10-30 02:00:57 +0900

    login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,7 +15,7 @@ export default async function LoginPage() {
     <div className="border-0 mx-auto max-w-md min-h-[80vh] px-4 py-24">
       <CardHeader className="space-y-1">
         <CardTitle className="text-2xl font-bold text-center text-gray-800">
-          우리가 찾는 모든 이벤트 Festivall
+          내 주변의 즐거움 Festivall
         </CardTitle>
       </CardHeader>
       <CardContent className="space-y-4 max-w-md px-16 mt-8">
