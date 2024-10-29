3c01626d398a4d2c265eb29990a9539d92462581
festiv-all <swhybeing@gmail.com>
2024-10-29 18:37:47 +0900

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,16 +9,16 @@ import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import InitUser from "@/lib/store/InitUser";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 // const notoSansKr = Noto_Sans_KR({
 //   weight: ["500"],
