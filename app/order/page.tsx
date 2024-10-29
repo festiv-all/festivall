f96765bbbe2f4ec5feffd894cf4472c8190c4f68
festiv-all <swhybeing@gmail.com>
2024-10-29 18:34:38 +0900

    order page

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -11,11 +11,13 @@ import {
 } from "@/components/ui/card";
 import useAttendeeStore from "@/lib/store/attendee";
 import useCartStore from "@/lib/store/cart";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter, useSearchParams } from "next/navigation";
+import { LoadingSpinner } from "@/components/icons/Icons";
 
 export default function OrderPage() {
+  const [loading, setLoading] = useState(false);
   const searchParams = useSearchParams();
   const router = useRouter();
   const event_title = useCartStore((state) => state.event_title);
@@ -48,7 +50,7 @@ export default function OrderPage() {
             </CardContent>
           </Card>
 
-          <OrderSummary />
+          <OrderSummary setLoading={setLoading} />
         </div>
       ) : (
         <div className="min-h-[80vh] mt-24">
@@ -57,6 +59,11 @@ export default function OrderPage() {
               No items in cart
             </CardTitle>
           </CardHeader>
+        </div>
+      )}
+      {loading && (
+        <div className="w-full h-full z-50 fixed top-0 left-0 flex justify-center items-center bg-opacity-30 bg-gray-100">
+          <LoadingSpinner className="h-12 w-12 animate-spin mb-36 text-gray-300" />
         </div>
       )}
     </div>
