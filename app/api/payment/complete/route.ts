f6cfea0e432e2031e18fba5d1a3cf1fc23bfe897
festiv-all <swhybeing@gmail.com>
2024-11-20 12:06:45 +0900

    payment complete api

--- app/api/payment/complete/route.ts
+++ app/api/payment/complete/route.ts
@@ -5,7 +5,7 @@ import { Attendee } from "@/lib/types/attendee";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 export async function POST(req: Request) {
--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -5,7 +5,7 @@ import { cartProduct } from "@/lib/types/cartProduct";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 export async function POST(req: Request) {
